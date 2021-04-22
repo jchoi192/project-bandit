8a96e641da60fe5d6cfef98fa4c18fc0df739763
Joseph Choi <jchoi192@macbook-pro.myfiosgateway.com>
2021-04-21 21:36:00 -0400

    feat: adds update and delete features

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,6 +4,9 @@ module.exports = {
     index,
     new: newPost,
     create,
+    delete: deletePost,
+    edit,
+    update,
 }
 
 function index(req, res) {
@@ -22,3 +25,29 @@ function create(req, res) {
         res.redirect('/posts')
     });
 }
+
+function deletePost(req, res) {
+    console.log(req.params.id, "THIS IS THE ID");
+    Tweet.findByIdAndDelete(req.params.id, function(err) {
+    res.redirect('/posts');
+    // Tweet.text.pull(req.params.id)
+    });
+};
+
+function edit(req, res) {
+    Tweet.findById(req.params.id, function(err, post) {
+        console.log(post.text, "is this post")
+        res.render('posts/edit', {
+            post: post,
+            postId: req.params.id
+        });
+    });
+}
+
+function update(req, res) {
+    console.log(req.params.id, "this is the ID")
+    console.log(req.body, "this is the BODYY")
+    Tweet.findByIdAndUpdate(req.params.id, req.body, function(err, updatedPost) {
+        res.redirect('/posts')
+    });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const postsCtrl = require('../controllers/posts')
 
-router.get('/', postsCtrl.index)
-router.get('/', postsCtrl.new)
-router.post('/', postsCtrl.create)
+router.get('/', postsCtrl.index);
+router.get('/', postsCtrl.new);
+router.post('/', postsCtrl.create);
+router.delete('/:id', postsCtrl.delete);
+router.get('/:id/edit', postsCtrl.edit)
+router.put('/:id', postsCtrl.update)
 
 module.exports = router;
